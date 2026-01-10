cb78ba072c54671dec52958b404d47e44efb069a
IgnasK <ignas.kunickas@gmail.com>
2026-01-10 22:16:36 +0200

    feat: update navbar and install icons lib

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -17,6 +17,7 @@ const Navbar = () => {
       <StyledNavList>
         {navigationItems.map((item) => (
           <li key={item.src}>
+            
             <StyledListNavItemAnch href={item.src}>
               {item.name}
             </StyledListNavItemAnch>
@@ -30,6 +31,9 @@ const Navbar = () => {
 export default Navbar;
 
 const StyledNavbar = styled.nav`
+  position: fixed;
+  bottom: 16px;
+
   display: flex;
   align-items: center;
   justify-content: space-between;
@@ -37,10 +41,20 @@ const StyledNavbar = styled.nav`
 
   height: 52px;
   width: 100%;
+  max-width: 450px;
 
   padding: 1rem 1.5rem;
 
-  background-color: #1f2937;
+  background: rgba(255, 255, 255, 0.18);
+  backdrop-filter: blur(20px) saturate(180%);
+  -webkit-backdrop-filter: blur(20px) saturate(180%);
+
+  border-radius: 20px;
+
+  box-shadow: 0 8px 30px rgba(0, 0, 0, 0.12),
+    inset 0 1px 0 rgba(255, 255, 255, 0.4);
+  color: red;
+  border: 1px solid rgba(255, 255, 255, 0.35);
 `;
 
 const StyledNavList = styled.ol`
@@ -53,11 +67,12 @@ const StyledNavList = styled.ol`
   width: 100%;
 
   margin: 0;
+  padding: 0;
 
   color: white;
 `;
 
 const StyledListNavItemAnch = styled.a`
-  color: white;
+  color: black;
   font-weight: bold;
 `;
